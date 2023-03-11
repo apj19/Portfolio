748e7754019b5ee133b83f0fa099bca14b53eaaf
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-03-11 15:03:55 +0530

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="bg-[url('./assets/main_bg.png')] min-w-[375px] max-w-[1440px] min-h-[100vh] bg-cover bg-center">
-      <h1 className="text-3xl font-bold underline">
-      Hello world! APJ
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world! APJ text123</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
